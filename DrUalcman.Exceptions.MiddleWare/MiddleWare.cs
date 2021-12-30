62b374cc36d15ae528c02c53888adb2e47094656
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-30 19:53:14 +0800

    add middlewares

---
+++ DrUalcman.Exceptions.MiddleWare/MiddleWare.cs
@@ -0,0 +1,35 @@
+﻿using DrUalcman.Exceptions.Interfaces;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace DrUalcman.Exceptions.MiddleWare;
+
+/// <summary>
+/// Transation Middleware
+/// </summary>
+class MiddleWare
+{
+    /// <summary>
+    /// Capture the exception and present like problem details
+    /// https://datatracker.ietf.org/doc/html/rfc7807
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="includeDetails"></param>
+    /// <param name="presenter"></param>
+    /// <returns></returns>
+    public static async Task WriteResponse(HttpContext context, bool includeDetails, IExceptionPresenter presenter)
+    {
+        IExceptionHandlerFeature exceptionDetail = context.Features.Get<IExceptionHandlerFeature>();
+        Exception exception = exceptionDetail.Error;
+
+        if (exception != null)
+        {
+            await presenter.Handle(exception, includeDetails);
+            context.Response.ContentType = "application/problem+json";
+            context.Response.StatusCode = (int)presenter.Content.Status;
+            var stream = context.Response.Body;
+            await JsonSerializer.SerializeAsync(stream, presenter.Content);
+        }
+    }
+}
---
+++ DrUalcman.Exceptions.MiddleWare/MiddleWareContainer.cs
@@ -0,0 +1,26 @@
+﻿using DrUalcman.Exceptions.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace DrUalcman.Exceptions.MiddleWare;
+
+/// <summary>
+/// inject middleware
+/// </summary>
+public static class MiddleWareContainer
+{
+    /// <summary>
+    /// Inject exception handle presneters
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="enviroment"></param>
+    /// <param name="presenter"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseExceptionHandlerPresenter(this IApplicationBuilder app, IHostEnvironment enviroment, IExceptionPresenter presenter)
+    {
+        app.Use((context, next) => MiddleWare.WriteResponse(context,
+            enviroment.IsDevelopment(), presenter));
+
+        return app;
+    }
+}
