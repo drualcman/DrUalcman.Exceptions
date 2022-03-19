e288abab5b35df0002be6485af542a3aa9f1fd28
Sergi Ortiz Gómez <drualcman@msn.com>
2022-03-19 20:22:03 +0800

    Fix a bug when the exception is not from DrUalcman.Exceptions.ProblemsDetailsException because is from ApiController ValidationException (for example) then don't show the correct invalidParamDetails. Add method EnsureSuccessCodeAsync() if want to use async actions

--- DrUalcman.Exceptions.Extensions/HttpResponseMessageExcensions.cs
+++ DrUalcman.Exceptions.Extensions/HttpResponseMessageExcensions.cs
@@ -1,10 +1,7 @@
-﻿using DrUalcman.Exceptions;
-using DrUalcman.Exceptions.Models;
-using System.Net.Http;
+﻿using DrUalcman.Exceptions.Models;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace DrUalcman.Exceptions.Extensions
 {
@@ -21,23 +18,65 @@ namespace DrUalcman.Exceptions.Extensions
         /// <exception cref="ProblemDetailsException"></exception>
         public static HttpResponseMessage EnsureSuccessCode(this HttpResponseMessage response)
         {
-            if (response.IsSuccessStatusCode) return response;
+            if(response.IsSuccessStatusCode) return response;
             else
             {
-                string responseContent = response.Content.ReadAsStringAsync().Result;
-                string invalidParams = Regex.Match(responseContent, "{[^{^}]*}").Value;                
-                IDictionary<string, string> keyValuePairs = new  Dictionary<string, string>();
-                if (!string.IsNullOrEmpty(invalidParams)) keyValuePairs = JsonSerializer.Deserialize<IDictionary<string, string>>(invalidParams);
-                ProblemDetails exception = JsonSerializer.Deserialize<ProblemDetails>(responseContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = false });
-                foreach (KeyValuePair<string, string> item in keyValuePairs)
+                ProblemDetails exception = response.Content.ReadFromJsonAsync<ProblemDetails>().Result;
+                if(exception.InvalidParams is null)
                 {
-                    exception.InvalidParams.TryAdd(item.Key, item.Value);
+                    string responseContent = response.Content.ReadAsStringAsync().Result;
+                    string invalidParams = Regex.Match(responseContent, "{[^{^}]*}").Value;
+                    if(!string.IsNullOrEmpty(invalidParams))
+                    {
+                        IDictionary<string, string[]> keyValuePairs = JsonSerializer.Deserialize<IDictionary<string, string[]>>(invalidParams)!;
+                        if(keyValuePairs != null)
+                        {
+                            exception.InvalidParams = new Dictionary<string, string>();
+                            foreach(KeyValuePair<string, string[]> item in keyValuePairs)
+                            {
+                                exception.InvalidParams.TryAdd(item.Key, string.Join('.', item.Value));
+                            }
+                        }
+                    }
                 }
 
                 throw new ProblemDetailsException(exception);
             }
         }
 
+        /// <summary>
+        /// If it's not a success code return a ProblemDetailsException
+        /// </summary>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        /// <exception cref="ProblemDetailsException"></exception>
+        public async static Task<HttpResponseMessage> EnsureSuccessCodeAsync(this HttpResponseMessage response)
+        {
+            if(response.IsSuccessStatusCode) return response;
+            else
+            {
+                ProblemDetails exception = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+                if(exception.InvalidParams is null)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    string invalidParams = Regex.Match(responseContent, "{[^{^}]*}").Value;
+                    if(!string.IsNullOrEmpty(invalidParams))
+                    {
+                        IDictionary<string, string[]> keyValuePairs = JsonSerializer.Deserialize<IDictionary<string, string[]>>(invalidParams)!;
+                        if(keyValuePairs != null)
+                        {
+                            exception.InvalidParams = new Dictionary<string, string>();
+                            foreach(KeyValuePair<string, string[]> item in keyValuePairs)
+                            {
+                                exception.InvalidParams.TryAdd(item.Key, string.Join('.', item.Value));
+                            }
+                        }
+                    }
+                }
+
+                throw new ProblemDetailsException(exception);
+            }
+        }
 
     }
 }
