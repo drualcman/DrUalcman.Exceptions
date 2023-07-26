384aafe61358924cf3aadd30d2f58a5b63cca39b
Sergi Ortiz Gómez <drualcman@msn.com>
2023-07-26 09:41:03 +0800

    Add new UnauthorizeException and Handler

---
+++ DrUalcman.Exceptions/Handlers/UnauthorizeExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using DrUalcman.Exceptions.Interfaces;
+using DrUalcman.Exceptions.Models;
+
+namespace DrUalcman.Exceptions.Handlers;
+
+/// <summary>
+/// Validation exception handler
+/// </summary>
+public class UnauthorizeExceptionHandler : IExceptionHandler<UnauthorizeException>
+{
+    /// <summary>
+    /// Validation excetion handle
+    /// </summary>
+    /// <param name="exception"></param>
+    /// <returns></returns>
+    public ValueTask<ProblemDetails> Handle(UnauthorizeException exception)
+    {
+        var ProblemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status401Unauthorized,
+            Type = StatusCodes.GetStatusCodeType(StatusCodes.Status401Unauthorized),
+            Title = "Unauthorized.",
+            Detail = string.IsNullOrEmpty(exception.Message) ? "You don't have access to the resource are you looking for." : exception.Message,
+            Instance = exception?.Source ?? "",
+            InvalidParams = new Dictionary<string, string>()
+        };
+        return ValueTask.FromResult(ProblemDetails);
+    }
+}
---
+++ DrUalcman.Exceptions/UnauthorizeException.cs
@@ -0,0 +1,26 @@
+﻿namespace DrUalcman.Exceptions;
+
+/// <summary>
+/// Generic Exception
+/// </summary>
+public class UnauthorizeException : Exception
+{
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public UnauthorizeException() { }
+
+    /// <summary>
+    /// Constructor with message
+    /// </summary>
+    /// <param name="message"></param>
+    public UnauthorizeException(string message) : base(message) { }
+
+    /// <summary>
+    /// Constructor with message and exception
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="innerException"></param>
+    public UnauthorizeException(string message, Exception innerException) : base(message, innerException) { }
+}
